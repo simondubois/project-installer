59daf6e8aaee85ea634b7b41a488e4f0fe853e01
Simon Dubois <simon@dubandubois.com>
2016-03-30 00:16:44 +0200

    phpmyadmin role

---
+++ roles/phpmyadmin/files/custom.config.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$cfg['Servers'][1]['auth_type'] = 'config';
+$cfg['Servers'][1]['user'] = 'root';
+$cfg['Servers'][1]['password'] = '';
+$cfg['Servers'][1]['AllowNoPassword'] = true;
+
+$cfg['Servers'][1]['hide_db'] = '^(information_schema|mysql|performance_schema|phpmyadmin)$';
