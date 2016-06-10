97f8afbca05bb54911e051b1205a31f3f35bfc65
Simon Dubois <simon@dubandubois.com>
2016-06-10 17:43:56 +0200

    Add options to phpmyadmin

--- roles/phpmyadmin/files/custom.config.inc.php
+++ roles/phpmyadmin/files/custom.config.inc.php
@@ -6,3 +6,6 @@ $cfg['Servers'][1]['password'] = '';
 $cfg['Servers'][1]['AllowNoPassword'] = true;
 
 $cfg['Servers'][1]['hide_db'] = '^(information_schema|mysql|performance_schema|phpmyadmin)$';
+
+$cfg['ShowAll'] = true;
+$cfg['RetainQueryBox'] = true;
